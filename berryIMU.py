d197c8f70c49e11a6ae070193f4ac71b5deb8751
BhavikJoshi <bhavik.joshi28@icloud.com>
2023-11-28 23:54:27 +0000

    Working IMU interface as a python package

---
+++ berryIMU.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python
+#
+#    This program  reads the angles from the acceleromteer, gyroscope
+#    and mangnetometer on a BerryIMU connected to a Raspberry Pi.
+#
+#    This program includes two filters (low pass and median) to improve the
+#    values returned from BerryIMU by reducing noise.
+#
+#    The BerryIMUv1, BerryIMUv2 and BerryIMUv3 are supported
+#
+#    This script is python 2.7 and 3 compatible
+#
+#    Feel free to do whatever you like with this code.
+#    Distributed as-is; no warranty is given.
+#
+#    http://ozzmaker.com/
+import sys
+import time
+import math
+import IMU
+import datetime
+import os
+import threading
+
+RAD_TO_DEG = 57.29578
+M_PI = 3.14159265358979323846
+G_GAIN = 0.070  # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
+AA =  0.40      # Complementary filter constant
+
+################# Compass Calibration values ############
+# Use calibrateBerryIMU.py to get calibration values
+# Calibrating the compass isnt mandatory, however a calibrated
+# compass will result in a more accurate heading values.
+
+
+'''
+Here is an example:
+magXmin =  -1748
+magYmin =  -1025
+magZmin =  -1876
+magXmax =  959
+magYmax =  1651
+magZmax =  708
+Dont use the above values, these are just an example.
+'''
+############### END Calibration offsets #################
+
+class MirrorMeIMU():
+
+    BUF_LEN = 10
+
+    def __init__(self):
+        IMU.detectIMU()     #Detect if BerryIMU is connected.
+        if(IMU.BerryIMUversion == 99):
+            print(" No BerryIMU found... exiting ")
+            sys.exit()
+        IMU.initIMU()       #Initialise the accelerometer, gyroscope and compass
+        self.buffer = []
+        self.thread = None
+        self.event = threading.Event()
+        self.event.clear()
+        self.time = datetime.datetime.now()
+    
+    def start(self):
+        self.event.set()
+        self.thread = threading.Thread(target=self.__run)
+        self.thread.start()
+
+    def stop(self):
+        self.event.clear()
+        self.thread.join()
+
+    def get_values(self):
+        return self.buffer
+
+    def __run(self):
+       magXmin =  0
+       magYmin =  0
+       magZmin =  0
+       magXmax =  0
+       magYmax =  0
+       magZmax =  0
+       gyroXangle = 0.0
+       gyroYangle = 0.0
+       gyroZangle = 0.0
+       CFangleX = 0.0
+       CFangleY = 0.0
+       while self.event.is_set():
+            #Read the accelerometer,gyroscope and magnetometer values
+            ACCx = IMU.readACCx()
+            ACCy = IMU.readACCy()
+            ACCz = IMU.readACCz()
+            GYRx = IMU.readGYRx()
+            GYRy = IMU.readGYRy()
+            GYRz = IMU.readGYRz()
+            MAGx = IMU.readMAGx()
+            MAGy = IMU.readMAGy()
+            MAGz = IMU.readMAGz()
+
+            #Apply compass calibration
+            MAGx -= (magXmin + magXmax) /2
+            MAGy -= (magYmin + magYmax) /2
+            MAGz -= (magZmin + magZmax) /2
+
+            ##Calculate loop Period(LP). How long between Gyro Reads
+            b = datetime.datetime.now() - self.time
+            self.time = datetime.datetime.now()
+            LP = b.microseconds/(1000000*1.0)
+
+            #Convert Gyro raw to degrees per second
+            rate_gyr_x =  GYRx * G_GAIN
+            rate_gyr_y =  GYRy * G_GAIN
+            rate_gyr_z =  GYRz * G_GAIN
+
+            #Calculate the angles from the gyro.
+            gyroXangle+=rate_gyr_x*LP
+            gyroYangle+=rate_gyr_y*LP
+            gyroZangle+=rate_gyr_z*LP
+
+            #Convert Accelerometer values to degrees
+            AccXangle =  (math.atan2(ACCy,ACCz)*RAD_TO_DEG)
+            AccYangle =  (math.atan2(ACCz,ACCx)+M_PI)*RAD_TO_DEG
+
+            #convert the values to -180 and +180
+            if AccYangle > 90:
+                AccYangle -= 270.0
+            else:
+                AccYangle += 90.0
+
+            #Complementary filter used to combine the accelerometer and gyro values.
+            CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle
+            CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
+
+            #Calculate heading
+            heading = 180 * math.atan2(MAGy,MAGx)/M_PI
+
+            #Only have our heading between 0 and 360
+            if heading < 0:
+                heading += 360
+
+            ####################################################################
+            ###################Tilt compensated heading#########################
+            ####################################################################
+            #Normalize accelerometer raw values.
+            accXnorm = ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+            accYnorm = ACCy/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+
+            #Calculate pitch and roll
+            pitch = math.asin(accXnorm)
+            roll = -math.asin(accYnorm/math.cos(pitch))
+
+            #Calculate the new tilt compensated values
+            #The compass and accelerometer are orientated differently on the the BerryIMUv1, v2 and v3.
+            #This needs to be taken into consideration when performing the calculations
+
+            #X compensation
+            if(IMU.BerryIMUversion == 1 or IMU.BerryIMUversion == 3):            #LSM9DS0 and (LSM6DSL & LIS2MDL)
+                magXcomp = MAGx*math.cos(pitch)+MAGz*math.sin(pitch)
+            else:                                                                #LSM9DS1
+                magXcomp = MAGx*math.cos(pitch)-MAGz*math.sin(pitch)
+
+            #Y compensation
+            if(IMU.BerryIMUversion == 1 or IMU.BerryIMUversion == 3):            #LSM9DS0 and (LSM6DSL & LIS2MDL)
+                magYcomp = MAGx*math.sin(roll)*math.sin(pitch)+MAGy*math.cos(roll)-MAGz*math.sin(roll)*math.cos(pitch)
+            else:                                                                #LSM9DS1
+                magYcomp = MAGx*math.sin(roll)*math.sin(pitch)+MAGy*math.cos(roll)+MAGz*math.sin(roll)*math.cos(pitch)
+
+
+            #Calculate tilt compensated heading
+            tiltCompensatedHeading = 180 * math.atan2(magYcomp,magXcomp)/M_PI
+
+            if tiltCompensatedHeading < 0:
+                tiltCompensatedHeading += 360
+
+            ##################### END Tilt Compensation ########################
+
+            values = [ACCx, ACCy, ACCz, GYRx, GYRy, GYRz, MAGx, MAGy, MAGz]
+            self.buffer.append(values)
+            if len(self.buffer) > MirrorMeIMU.BUF_LEN:
+                self.buffer.pop(0)
+
+            #slow program down a bit, makes the output more readable
+            time.sleep(0.1)
--- pi.py
+++ pi.py
@@ -1,18 +1,11 @@
-from BerryIMU.python_BerryIMU_gyro_accel_compass_filters.berryIMU import *
-from BerryIMU.python_BerryIMU_gyro_accel_compass_filters.IMU import *
+from berryIMU import *
 import time
 def main():
     imu = MirrorMeIMU()
     imu.start()
-    time.sleep(2)
-    print(imu.get_values())
-    time.sleep(2)
-    print(imu.get_values())
+    for i in range(0, 1000):
+        print(imu.get_values())
     imu.stop()
-    time.sleep(3)
-    print(imu.get_values())
-    time.sleep(1)
-    print(imu.get_values())
 
 if __name__ == "__main__":
     main()
