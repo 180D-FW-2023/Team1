05a1d75a49f4b26765d8913650a882440c138a37
Derek Hua <derekhua@ucla.edu>
2023-11-29 16:37:01 -0800

    Added numbers and lines

--- model_utils.py
+++ model_utils.py
@@ -56,10 +56,25 @@ def point_overlay(image):
     keypoints_with_scores = movenet(resized_image)
 
     image = np.array(image[0]).astype(np.uint8)
-
+    points = {x: None for x in range(17)}
     for i, (y, x, score) in enumerate(keypoints_with_scores[0][0]):
         #print(x, y)
         if score > 0.4:
-            image = cv2.circle(image, (int(x * image.shape[1]), int(y * image.shape[0])), 20, (0, 0, 255), -1)
-            image = cv2.putText(image, str(i), (int(x * image.shape[1]), int(y * image.shape[0])), font, fontScale, color, thickness, cv2.LINE_AA) 
+            x = int(x * image.shape[1])
+            y = int(y * image.shape[0])
+            points[i] = (x, y)
+            image = cv2.circle(image, (x, y), 20, (0, 0, 255), -1)
+            image = cv2.putText(image, str(i), (x, y), font, fontScale, color, thickness, cv2.LINE_AA)
+        else:
+            points[i] = None
+    if points[5] and points[7]:
+      image = cv2.line(image, points[5], points[7], (255,0,0), 5) 
+    if points[7] and points[9]:
+      image = cv2.line(image, points[7], points[9], (255,0,0), 5) 
+
+    if points[6] and points[8]:
+      image = cv2.line(image, points[6], points[8], (255,0,0), 5) 
+    if points[8] and points[10]:
+      image = cv2.line(image, points[8], points[10], (255,0,0), 5) 
+    
     return cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
