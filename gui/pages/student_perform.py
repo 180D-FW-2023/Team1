019f6e8f57c3609f677360cbbedb779832355a20
Bhavik <bhavik.joshi28@icloud.com>
2024-01-30 22:07:08 -0800

    Add opencv frame to recording and performing page

--- gui/pages/student_perform.py
+++ gui/pages/student_perform.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
+import cv2
+import time
 
 # Change session state based on page
 st.session_state['current_page'] = 'student_perform'
+FPS = 30
+
 if 'received_movement' not in st.session_state:
     st.session_state['received_movement'] = False
 
@@ -14,14 +18,37 @@ def on_recv():
     '''
     pass
 
-def render_student_start():
+def render_student_perform():
     st.title("Welcome to MirrorMe!")
     st.header("Waiting for teacher to send movement")
-    if st.button("Exit"):
-        # TODO: cleanup mqtt object
-        switch_page("home")
-    if st.session_state['received_movement']:
-        if st.button("Start / Stop"):
+    frame_holder = st.empty()
+    fps_counter = st.empty()
+    exit_button = st.button("Exit")
+    start_stop_button = st.empty()
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        # Get loop start time
+        loop_start = time.monotonic_ns()
+        # Get frame
+        ret, frame = cap.read()
+        if not ret:
+            break
+        # Modify frame for viewing (flip, color, scaling)
+        frame = cv2.flip(frame, 1)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, (frame.shape[1]//2, frame.shape[0]//2))
+        frame_holder.image(frame)
+        # Spin loop to get 1/FPS FPS
+        while time.monotonic_ns() < loop_start + (1/FPS*1_000_000_000):
             pass
+        # Update FPS counter
+        fps_counter.markdown(f"FPS: {str(1_000_000_000.0 / (time.monotonic_ns() - loop_start))}")
+        # Handle Buttons
+        if exit_button:
+            cap.release()
+            st.session_state['mqtt'].loop_stop()
+            st.session_state['mqtt'].disconnect()
+            switch_page("home")
+    cap.release()
 
-render_student_start()
+render_student_perform()
--- gui/pages/teacher_record.py
+++ gui/pages/teacher_record.py
@@ -1,13 +1,20 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
+import cv2
+import time
+import movement
+from model_utils import *
 
 # Change session state based on page
 st.session_state['current_page'] = 'teacher_record'
+
+if 'recording' not in st.session_state:
+    st.session_state['recording'] = False
+
 if 'movement_recorded' not in st.session_state:
     st.session_state['movement_recorded'] = False
 
-# TODO: mqtt: - become main publisher of teacher_room_code
-#             - subscibe to student_room_code
+FPS = 30
 
 def on_recv():
     '''
@@ -22,17 +29,52 @@ def on_recv():
 def render_teacher_record():
     st.title("Record your Movement for your Students!")
     st.header(" ")
-    st.header("Students:" )
-    if st.button("Exit"):
-        st.session_state['mqtt'].loop_stop()
-        st.session_state['mqtt'].disconnect()
-        switch_page("home")
+    cap = cv2.VideoCapture(0)
+    frame_holder = st.empty()
+    fps_counter = st.empty()
+    exit_button = st.button("Exit")
     recording_button = st.empty()
-    recording_button.button('Record')
-    if st.button("Send"):
-        st.session_state['mqtt'].publish('ece180d/test', "joe", qos=1)
-        recording_button.empty()
-        recording_button.button('Rerecord')
-        st.session_state['movement_recorded'] = True
+    record = recording_button.button('Record', key='record')
+    send_button = st.button("Send", disabled=True)
+    while cap.isOpened():
+        # Get loop start time
+        loop_start = time.monotonic_ns()
+        # Get frame
+        ret, frame = cap.read()
+        if not ret:
+            break
+        # Modify frame for viewing (flip, color, scaling)
+        frame = cv2.flip(frame, 1)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, (frame.shape[1]//2, frame.shape[0]//2))
+        # If in idle mode
+            # If have sent a recording before, play the recording, if not, then just display video
+        # If in record mode
+            # Record movement
+        frame_holder.image(frame)
+        # Spin loop to get 1/FPS FPS
+        while time.monotonic_ns() < loop_start + (1/FPS*1_000_000_000):
+            pass
+        # Update FPS Counter
+        fps_counter.markdown(f"FPS: {str(1_000_000_000.0 / (time.monotonic_ns() - loop_start))}")
+        # Handle Buttons
+        if exit_button:
+            cap.release()
+            st.session_state['mqtt'].loop_stop()
+            st.session_state['mqtt'].disconnect()
+            switch_page("home")
+        if record:
+            if st.session_state['recording'] == False:
+                st.session_state['recording'] = True
+                record = recording_button.button("Stop", key=f"stop{str(time.monotonic_ns())}")
+            else:
+                st.session_state['recording'] = False
+                st.session_state['movement_recorded'] = True
+                record = recording_button.button("Rerecord", key=f"rerecord{str(time.monotonic_ns())}")
+            
+        if send_button:
+            st.session_state['mqtt'].publish('ece180d/test', "joe", qos=1)
+            st.session_state['movement_recorded'] = True
+    cap.release()
 
 render_teacher_record()
